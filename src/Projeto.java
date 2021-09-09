bec171cd481fb15e82e4fa7efd889457ce316234
Diego-k2 <degosantosiva@gmail.com>
2021-09-09 11:30:58 -0300

    adiciona laço de repetição

--- src/Projeto.java
+++ src/Projeto.java
@@ -6,18 +6,23 @@ public class Projeto {
 		
 		Scanner entrada = new Scanner(System.in); 
 		Medidas dados = new Medidas();
+	
+		System.out.print("Iniciar ou Sair: " );
+		String op = entrada.next();
 		
-		System.out.print("Entre com o seu peso:" );
-		dados.peso = entrada.nextDouble();
-		System.out.print("Entre com sua altura em Metros: ");
-		dados.altura = entrada.nextDouble();
-		
-		dados.imc();
-		
-		
+		while (!op.toLowerCase().trim().equals("sair")) {
+			System.out.print("Entre com o seu peso:" );
+			dados.peso = entrada.nextDouble();
+			System.out.print("Entre com sua altura em Metros: ");
+			dados.altura = entrada.nextDouble();
+				
+			dados.imc();
+			
+			System.out.print("Continuar ou Sair: ");
+			op = entrada.next();
+		}
+			
 		entrada.close();
-		
-		
 	}
 	
 }
